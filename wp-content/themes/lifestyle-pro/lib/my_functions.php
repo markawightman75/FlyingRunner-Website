153831e571b890a84aa38724a4554b1092cc7eb2
markwightman <markawightman@gmail.com>
2016-02-19 15:40:16 +0000

    Add OpenGraph tags (for Facebook etc.) - turn off Yoast social setting that does this

--- wp-content/themes/lifestyle-pro/lib/my_functions.php
+++ wp-content/themes/lifestyle-pro/lib/my_functions.php
@@ -30,6 +30,11 @@ function add_custom_widgets() {
 add_action( 'widgets_init', 'add_custom_widgets' );
 
 add_action( 'genesis_after_entry_content', 'add_pacing_calculator_javascript' );
+
+// Add OpenGraph tags (for Facebook etc. to head) - turn off Yoast settings that do this
+add_filter('language_attributes', 'doctype_opengraph');
+add_action('wp_head', 'fb_opengraph', 5);
+
 function add_pacing_calculator_javascript() {
 	if ( get_the_title() == "Explore Marathon Research Data" )	{
 		//Register the javascript file that contains client-side logic
@@ -693,4 +698,66 @@ function hide_shipping_when_free_is_available( $rates, $package ) {
 	return $rates;
 }
 
-?>
+function doctype_opengraph($output) {
+    return $output . '
+    xmlns:og="http://opengraphprotocol.org/schema/"
+    xmlns:fb="http://www.facebook.com/2008/fbml"';
+}
+function fb_opengraph() {
+    global $post;
+ 
+    if(is_single()) {
+        $thumb_url = '';
+		if( class_exists('Dynamic_Featured_Image') ) {
+			global $dynamic_featured_image;
+			$featured_images = $dynamic_featured_image->get_featured_images( );
+			$featured_image = $featured_images[0];
+			$thumb_url = $featured_image['full'];
+		   //You can now loop through the image to display them as required
+		}
+		else
+		{
+			//Get the url of the featured image
+			$thumb_id = get_post_thumbnail_id();
+			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+			$thumb_url = $thumb_url_array[0];
+		}
+		
+		
+		if (substr($thumb_url,0, 19) != "http://127.0.0.1:82" )
+		{
+			//Redirect to http://images.
+			$firstdot = strpos($thumb_url, ".");
+			$url_secondpart = substr($thumb_url, $firstdot+1);
+			$thumb_url = "http://images." . $url_secondpart;
+			$thumb_url = str_replace("2/wp-content/uploads/", "",$thumb_url);
+			$thumb_url = str_replace("wp-content/uploads/", "",$thumb_url);
+			$thumb_url = str_replace("images.flyingrunner.co.uk/2/", "images.flyingrunner.co.uk/",$thumb_url);
+		}
+		
+		if (! empty ($thumb_url)) {						
+			$img_src = $thumb_url;
+		} else {
+			$img_src = "http://images.flyingrunner.co.uk/2014/07/RunnersHorizon-banner10.jpg";
+		}
+		
+        if($excerpt = $post->post_excerpt) {
+            $excerpt = strip_tags($post->post_excerpt);
+            $excerpt = str_replace("", "'", $excerpt);
+        } else {
+            $excerpt = get_bloginfo('description');
+        }
+        ?>
+ 
+    <meta property="og:title" content="<?php echo the_title(); ?>"/>
+    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+    <meta property="og:image" content="<?php echo $img_src; ?>"/>
+ 
+<?php
+    } else {
+        return;
+    }
+}
